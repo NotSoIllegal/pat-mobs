e618fd5b0d1618ab0363343eab1e4072ab0ed5d5
NotSoIllegal <gilya9448@gmail.com>
2025-07-11 16:27:18 +0600

    Effect offset for each mob

--- BP/scripts/config.js
+++ BP/scripts/config.js
@@ -1,33 +1,111 @@
-export const pettableMobs = [
-    "minecraft:cat",
-    "minecraft:ocelot",
-    "minecraft:wolf",
-    "minecraft:axolotl",
-    "minecraft:rabbit",
-    "minecraft:fox",
-    "minecraft:sheep",
-    "minecraft:parrot",
-    "minecraft:sniffer",
-    "minecraft:panda",
-    "minecraft:horse",
-    "minecraft:donkey",
-    "minecraft:llama",
-    "minecraft:zombie",
-    "minecraft:spider",
-    "minecraft:skeleton",
-    "minecraft:pig",
-    "minecraft:cow",
-    "minecraft:sniffer",
-    "minecraft:fox",
-    "minecraft:frog",
-    "minecraft:witch",
+export const pattableMobs = {
+    "minecraft:cat": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:ocelot": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:creeper": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:skeleton": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:witch": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:enderman": {
+        "particleOffset":
+            [0, 1.5, 0]
+    },
+    "minecraft:wolf": {},
+    "minecraft:pig": {},
+    "minecraft:spider": {},
+    "minecraft:zombie": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:axolotl": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:rabbit": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:fox": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:sheep": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:parrot": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:sniffer": {
+        "particleOffset":
+            [0, 1.25, 0]
+    },
+    "minecraft:panda": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:horse": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:donkey": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:llama": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:cow": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:frog": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
     //"minecraft:villager", # on hold
-    "minecraft:turtle",
-    "minecraft:squid",
-    "minecraft:chicken",
-    "minecraft:armadillo",
-    "minecraft:drowned",
-    "minecraft:bee",
-    "minecraft:dolphin",
-    "minecraft:ghast"
-];
+    "minecraft:turtle": {
+        "particleOffset":
+            [0, -0.25, 0]
+    },
+    "minecraft:squid": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:chicken": {},
+    "minecraft:armadillo": {},
+    "minecraft:drowned": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:bee": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:dolphin": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:ghast": {
+        "particleOffset":
+            [0, 3.5, 0]
+    },
+
+}
+
+export var forceSneakForPat = true;

b0a60cd9faa122b98ea611474357b0b425827871
NotSoIllegal <gilya9448@gmail.com>
2025-07-11 16:27:50 +0600

    Working with particle offset now
    Todo is still toggle sneak option

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -2,20 +2,24 @@ import {
     world,
     system
 } from "@minecraft/server";
-import { pettableMobs } from './config.js';
+import { pattableMobs } from './config.js';
 
 world.beforeEvents.playerInteractWithEntity.subscribe((event) => {
     const player = event.player;
     const entity = event.target;
 
-    if (!player.isSneaking) return; //Todo sneaking patting toggle option 
+    const mob = pattableMobs[entity.typeId];
+    const [px, py, pz] = mob ? mob.particleOffset ?? [0, 0, 0] : [0, 0, 0];
 
-    if (pettableMobs.includes(entity.typeId)) {
+    //if (!player.isSneaking && forceSneakForPat) return;
+    if (!player.isSneaking) return;
+
+    if (mob) {
         event.cancel = true;
         system.run(() => {
             entity.playAnimation("animation.pat");
-            entity.runCommand(`execute at @s run particle pattmobs:hand ~ ~ ~`);
-            entity.runCommand(`execute at @s run playsound random.pop @a ~ ~ ~`);
+            entity.runCommand(`execute at @s run particle pattmobs:hand ~${px} ~${py} ~${pz}`);
+            entity.runCommand(`execute at @s run playsound pattmobs:pat @a ~ ~ ~`);
         })
     }
 });

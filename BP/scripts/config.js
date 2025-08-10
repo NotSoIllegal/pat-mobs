a257a12d7b9ca9d82df1c55f1cbd467b6418a3c4
Jesta <gilya9448@gmail.com>
2025-08-10 18:04:53 +0600

    (almost) all mobs

--- BP/scripts/config.js
+++ BP/scripts/config.js
@@ -3,10 +3,7 @@ export const pattableMobs = {
         "particleOffset":
             [0, -0.5, 0]
     },
-    "minecraft:ocelot": {
-        "particleOffset":
-            [0, 0.5, 0]
-    },
+    "minecraft:ocelot": {},
     "minecraft:creeper": {
         "particleOffset":
             [0, 1, 0]
@@ -15,7 +12,59 @@ export const pattableMobs = {
         "particleOffset":
             [0, 1, 0]
     },
+    "minecraft:bogged": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:zombie_villager": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:vindicator": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:evocation_illager": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:pillager": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:blaze": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:wither_skeleton": {
+        "particleOffset":
+            [0, 1.5, 0]
+    },
+    "minecraft:slime": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:magma_cube": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:hoglin": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:zoglin": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:strider": {
+        "particleOffset":
+            [0, 0, 0]
+    },
     "minecraft:witch": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:breeze": {
         "particleOffset":
             [0, 1, 0]
     },
@@ -23,14 +72,84 @@ export const pattableMobs = {
         "particleOffset":
             [0, 1.5, 0]
     },
+    "minecraft:warden": {
+        "particleOffset":
+            [0, 2.5, 0]
+    },
+    "minecraft:creaking": {
+        "particleOffset":
+            [0, 2, 0]
+    },
+    "minecraft:ravager": {
+        "particleOffset":
+            [0, 2, 0]
+    },
+    "minecraft:iron_golem": {
+        "particleOffset":
+            [0, 2, 0]
+    },
     "minecraft:wolf": {},
     "minecraft:pig": {},
+    "minecraft:shulker": {
+        "particleOffset":
+            [0, 0.25, 0]
+    },
     "minecraft:spider": {},
+    "minecraft:bat": {},
+    "minecraft:cave_spider": {},
     "minecraft:zombie": {
         "particleOffset":
             [0, 1, 0]
     },
+    "minecraft:snow_golem": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:piglin": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:piglin_brute": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:guardian": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:elder_guardian": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:husk": {
+        "particleOffset":
+            [0, 1, 0]
+    },
+    "minecraft:zombie_pigman": {
+        "particleOffset":
+            [0, 1, 0]
+    },
     "minecraft:axolotl": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:silverfish": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:phantom": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:vex": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:allay": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:endermite": {
         "particleOffset":
             [0, -0.5, 0]
     },
@@ -70,7 +189,35 @@ export const pattableMobs = {
         "particleOffset":
             [0, 0.5, 0]
     },
+    "minecraft:trader_llama": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
     "minecraft:cow": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:goat": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:mule": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:skeleton_horse": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:zombie_horse": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:mooshroom": {
+        "particleOffset":
+            [0, 0.5, 0]
+    },
+    "minecraft:polar_bear": {
         "particleOffset":
             [0, 0.5, 0]
     },
@@ -84,6 +231,10 @@ export const pattableMobs = {
             [0, -0.25, 0]
     },
     "minecraft:squid": {
+        "particleOffset":
+            [0, -0.5, 0]
+    },
+    "minecraft:glow_squid": {
         "particleOffset":
             [0, -0.5, 0]
     },
@@ -102,6 +253,10 @@ export const pattableMobs = {
             [0, -0.5, 0]
     },
     "minecraft:ghast": {
+        "particleOffset":
+            [0, 3.5, 0]
+    },
+    "minecraft:happy_ghast": {
         "particleOffset":
             [0, 3.5, 0]
     },
